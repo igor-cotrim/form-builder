c6af5234838b5b6678daacee6d48633f3c12c100
Igor Cotrim <igorcotrim.dev@gmail.com>
2024-01-03 14:55:13 -0300

    feat: create logic to drag and drop elements in Drop area

--- src/components/Designer.tsx
+++ src/components/Designer.tsx
@@ -9,8 +9,13 @@ import { ElementsType } from "@/models";
 import { DesignerElementWrapper, DesignerSidebar, FormElements } from ".";
 
 function Designer() {
-  const { elements, addElement, selectedElement, setSelectedElement } =
-    useDesigner();
+  const {
+    elements,
+    addElement,
+    selectedElement,
+    setSelectedElement,
+    removeElement,
+  } = useDesigner();
 
   const droppable = useDroppable({
     id: "designer-drop-area",
@@ -26,14 +31,82 @@ function Designer() {
       if (!active || !over) return;
 
       const isDesignerBtnElement = active.data?.current?.isDesignerBtnElement;
+      const isDroppingOverDesignerDropArea =
+        over.data?.current?.isDesignerDropArea;
 
-      if (isDesignerBtnElement) {
+      const droppingSidebarBtnOverDesignerDropArea =
+        isDesignerBtnElement && isDroppingOverDesignerDropArea;
+
+      if (droppingSidebarBtnOverDesignerDropArea) {
         const type = active.data?.current?.type;
         const newElement = FormElements[type as ElementsType].constructor(
           idGenerator()
         );
 
-        addElement(0, newElement);
+        addElement(elements.length, newElement);
+
+        return;
+      }
+
+      const isDroppingOverDesignerElementTopHalf =
+        over.data?.current?.isTopHalfDesignerElement;
+      const isDroppingOverDesignerElementBottomHalf =
+        over.data?.current?.isBottomHalfDesignerElement;
+      const isDroppingOverDesignerElement =
+        isDroppingOverDesignerElementTopHalf ||
+        isDroppingOverDesignerElementBottomHalf;
+      const droppingSidebarBtnOverDeisgnerElement =
+        isDesignerBtnElement && isDroppingOverDesignerElement;
+
+      if (droppingSidebarBtnOverDeisgnerElement) {
+        const type = active.data?.current?.type;
+        const newElement = FormElements[type as ElementsType].constructor(
+          idGenerator()
+        );
+        const overId = over.data?.current?.elementId;
+        const overElementIndex = elements.findIndex(
+          (element) => element.id === overId
+        );
+
+        if (overElementIndex === -1) throw new Error("Element not found");
+
+        let indexForNewElement = overElementIndex;
+
+        if (isDroppingOverDesignerElementBottomHalf) indexForNewElement++;
+
+        addElement(indexForNewElement, newElement);
+
+        return;
+      }
+
+      const isDraggingDesignerElement = active.data?.current?.isDesignerElement;
+      const draggingDesignerElementOverAnotherDesignerElement =
+        isDroppingOverDesignerElement && isDraggingDesignerElement;
+
+      if (draggingDesignerElementOverAnotherDesignerElement) {
+        const activeId = active.data?.current?.elementId;
+        const overId = over.data?.current?.elementId;
+
+        const activeElementIndex = elements.findIndex(
+          (element) => element.id === activeId
+        );
+        const overElementIndex = elements.findIndex(
+          (element) => element.id === overId
+        );
+
+        if (activeElementIndex === -1 || overElementIndex === -1) {
+          throw new Error("Element not found");
+        }
+
+        const activeElement = { ...elements[activeElementIndex] };
+
+        removeElement(activeId);
+
+        let indexForNewElement = overElementIndex;
+
+        if (isDroppingOverDesignerElementBottomHalf) indexForNewElement++;
+
+        addElement(indexForNewElement, activeElement);
       }
     },
   });
@@ -50,7 +123,7 @@ function Designer() {
           ref={droppable.setNodeRef}
           className={cn(
             "bg-background max-w-[920px] w-full h-full m-auto rounded-xl flex flex-col flex-grow items-center justify-start flex-1 overflow-y-auto",
-            droppable.isOver && "ring-2 ring-primary/20"
+            droppable.isOver && "ring-4 ring-primary ring-inset"
           )}
         >
           {!droppable.isOver && elements.length === 0 && (
